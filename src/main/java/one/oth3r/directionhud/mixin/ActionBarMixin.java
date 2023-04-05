7a70f4aa7d50638e0ce634897ce0dcb532f3302c
Oth3r <oth3rone@outlook.com>
2023-04-05 18:46:04 -0500

    found the better solution

--- src/main/java/one/oth3r/directionhud/mixin/ActionBarMixin.java
+++ src/main/java/one/oth3r/directionhud/mixin/ActionBarMixin.java
@@ -2,31 +2,31 @@ package one.oth3r.directionhud.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.text.ClickEvent;
 import net.minecraft.text.Text;
+import one.oth3r.directionhud.DirectionHUD;
 import one.oth3r.directionhud.DirectionHUDClient;
-import one.oth3r.directionhud.commands.HUD;
+import one.oth3r.directionhud.files.PlayerData;
 import one.oth3r.directionhud.utils.CUtl;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.Objects;
-
 @Mixin(InGameHud.class)
 public class ActionBarMixin {
     @Inject(at = @At("HEAD"), method = "setOverlayMessage(Lnet/minecraft/text/Text;Z)V")
     private void sendMessage(Text message, boolean tinted, CallbackInfo info) {
         MinecraftClient client = MinecraftClient.getInstance();
-        if (!client.isInSingleplayer() && !DirectionHUDClient.onSupportedServer) return;
-        if (HUD.lastBar.getString().equals("") || message.getString().equals("")) return;
-        if (!Objects.equals(HUD.lastBar.getString().charAt(0), message.getString().charAt(0))) {
-            System.out.println(HUD.lastBar.getString());
-            System.out.println(message.getString());
-            //when the client is lagging, it might put the hud's message in chat, also if the first char in the hud matches the actionhud it also might mess up
-            // theres probably a better way to do it, ill think bout it
-            assert client.player != null;
-            client.player.sendMessage(CUtl.tag(message));
+        ClickEvent click = message.getStyle().getClickEvent();
+        if (click == null || !click.getValue().equals("https://modrinth.com/mod/directionhud")) {
+            if (client.player != null) {
+                if (client.isInSingleplayer() && PlayerData.get.hud.state(DirectionHUD.server.getPlayerManager().getPlayer(client.player.getUuid()))) {
+                    client.player.sendMessage(CUtl.tag(message));
+                } else if (DirectionHUDClient.onSupportedServer && DirectionHUDClient.hudState) {
+                    client.player.sendMessage(CUtl.tag(message));
+                }
+            }
         }
     }
 }
