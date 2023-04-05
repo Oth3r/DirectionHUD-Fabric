cfd3226706efaa4881f9bbb62e928b1bd3188108
Oth3r <oth3rone@outlook.com>
2023-04-05 14:04:49 -0500

    fixed some issues with ActionBarMixin

--- src/main/java/one/oth3r/directionhud/mixin/ActionBarMixin.java
+++ src/main/java/one/oth3r/directionhud/mixin/ActionBarMixin.java
@@ -3,7 +3,7 @@ package one.oth3r.directionhud.mixin;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.text.Text;
-import one.oth3r.directionhud.DirectionHUD;
+import one.oth3r.directionhud.DirectionHUDClient;
 import one.oth3r.directionhud.commands.HUD;
 import one.oth3r.directionhud.utils.CUtl;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,10 +17,14 @@ import java.util.Objects;
 public class ActionBarMixin {
     @Inject(at = @At("HEAD"), method = "setOverlayMessage(Lnet/minecraft/text/Text;Z)V")
     private void sendMessage(Text message, boolean tinted, CallbackInfo info) {
-        if (!Objects.equals(HUD.lastBar.getString().charAt(1), message.getString().charAt(1)) && message != Text.of("")) {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (!client.isInSingleplayer() && !DirectionHUDClient.onSupportedServer) return;
+        if (HUD.lastBar.getString().equals("") || message.getString().equals("")) return;
+        if (!Objects.equals(HUD.lastBar.getString().charAt(0), message.getString().charAt(0))) {
+            System.out.println(HUD.lastBar.getString());
+            System.out.println(message.getString());
             //when the client is lagging, it might put the hud's message in chat, also if the first char in the hud matches the actionhud it also might mess up
             // theres probably a better way to do it, ill think bout it
-            MinecraftClient client = MinecraftClient.getInstance();
             assert client.player != null;
             client.player.sendMessage(CUtl.tag(message));
         }
