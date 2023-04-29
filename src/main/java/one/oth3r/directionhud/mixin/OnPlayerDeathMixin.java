2095dc110b2e43d987c26c2579f452d0ae6b222c
Oth3r <oth3rone@outlook.com>
2023-04-29 11:01:10 -0500

    better saved death message

--- src/main/java/one/oth3r/directionhud/mixin/OnPlayerDeathMixin.java
+++ src/main/java/one/oth3r/directionhud/mixin/OnPlayerDeathMixin.java
@@ -3,6 +3,7 @@ package one.oth3r.directionhud.mixin;
 import one.oth3r.directionhud.commands.Destination;
 import one.oth3r.directionhud.files.PlayerData;
 import one.oth3r.directionhud.files.config;
+import one.oth3r.directionhud.utils.CTxT;
 import one.oth3r.directionhud.utils.CUtl;
 import one.oth3r.directionhud.utils.Utl;
 import net.minecraft.entity.damage.DamageSource;
@@ -19,8 +20,11 @@ public class OnPlayerDeathMixin {
         ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
         if (!config.deathsaving || !PlayerData.get.dest.setting.lastdeath(player)) return;
         Destination.lastdeath.add(player, Utl.player.dim(player), Utl.player.XYZ(player));
-        player.sendMessage(CUtl.tag().append(CUtl.lang("dest.lastdeath.save"))
-                .append(" ")
-                .append(CUtl.CButton.dest.set("/dest set "+Utl.player.XYZ(player))).b());
+        CTxT msg = CUtl.tag().append(CUtl.lang("dest.lastdeath.save"))
+                .append(" ").append(CUtl.xyzBadge(Utl.player.XYZ(player),Utl.player.dim(player),null,null))
+                .append(" ").append(CUtl.CButton.dest.set("/dest set "+Utl.player.XYZ(player)));
+        if (Utl.dim.showConvertButton(Utl.dim.format(player.getSpawnPointDimension().getValue()),Utl.player.dim(player)))
+            msg.append(" ").append(CUtl.CButton.dest.convert("/dest set "+Utl.player.XYZ(player)+" "+Utl.player.dim(player)));
+        player.sendMessage(msg.b());
     }
 }
