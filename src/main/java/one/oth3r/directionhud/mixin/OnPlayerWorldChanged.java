5ec38fe83de86d8c0345c55b0b532a75b04c0902
Oth3r <oth3rone@outlook.com>
2023-05-09 17:59:06 -0500

    new Loc support

--- src/main/java/one/oth3r/directionhud/mixin/OnPlayerWorldChanged.java
+++ src/main/java/one/oth3r/directionhud/mixin/OnPlayerWorldChanged.java
@@ -6,6 +6,7 @@ import one.oth3r.directionhud.commands.Destination;
 import one.oth3r.directionhud.files.PlayerData;
 import one.oth3r.directionhud.utils.CTxT;
 import one.oth3r.directionhud.utils.CUtl;
+import one.oth3r.directionhud.utils.Loc;
 import one.oth3r.directionhud.utils.Utl;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -17,13 +18,13 @@ public class OnPlayerWorldChanged {
     @Inject(at = @At("HEAD"), method = "worldChanged(Lnet/minecraft/server/world/ServerWorld;)V")
     public void worldChangedCallback(ServerWorld world, CallbackInfo info) {
         ServerPlayerEntity player = (ServerPlayerEntity) (Object) this;
-        if (PlayerData.get.dest.setting.autoclear(player) && !Destination.get(player, "xyz").equals("f")) {
-            String dest = Destination.get(player,"xyz");
+        if (PlayerData.get.dest.setting.autoclear(player) && Destination.get(player).hasXYZ()) {
+            Loc loc = Destination.get(player);
             String dim = world.getRegistryKey().getValue().getPath();
             CTxT msg = CTxT.of("").append(CUtl.lang("dest.cleared_dim").color('7').italic(true))
-                    .append(" ").append(CUtl.CButton.dest.set("/dest set "+dest));
+                    .append(" ").append(CUtl.CButton.dest.set("/dest set "+loc.getXYZ()));
             if (Utl.dim.showConvertButton(Utl.player.dim(player),dim))
-                msg.append(" ").append(CUtl.CButton.dest.convert("/dest set "+dest+" "+dim));
+                msg.append(" ").append(CUtl.CButton.dest.convert("/dest set "+loc.getXYZ()+" "+dim));
             Destination.clear(player, msg);
         }
     }
