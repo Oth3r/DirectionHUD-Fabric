3d0ff6804d47b58f905caad92d3ddb957ecbe9a0
Oth3r <oth3rone@outlook.com>
2023-06-20 21:00:07 -0500

    Chat message from actionbar update

--- src/main/java/one/oth3r/directionhud/mixin/ActionBarMixin.java
+++ src/main/java/one/oth3r/directionhud/mixin/ActionBarMixin.java
@@ -3,11 +3,12 @@ package one.oth3r.directionhud.mixin;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.text.ClickEvent;
+import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 import one.oth3r.directionhud.DirectionHUD;
 import one.oth3r.directionhud.DirectionHUDClient;
 import one.oth3r.directionhud.common.files.PlayerData;
-import one.oth3r.directionhud.utils.CUtl;
+import one.oth3r.directionhud.utils.CTxT;
 import one.oth3r.directionhud.utils.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,6 +21,7 @@ import java.util.Objects;
 public class ActionBarMixin {
     @Inject(at = @At("HEAD"), method = "setOverlayMessage(Lnet/minecraft/text/Text;Z)V")
     private void sendMessage(Text message, boolean tinted, CallbackInfo info) {
+        CTxT msg = CTxT.of((MutableText) message).hEvent(CTxT.of("Message from the actionbar sent by DirectionHUD."));
         MinecraftClient client = MinecraftClient.getInstance();
         ClickEvent click = message.getStyle().getClickEvent();
         if (click == null || !click.getValue().equals("https://modrinth.com/mod/directionhud")) {
@@ -27,9 +29,9 @@ public class ActionBarMixin {
             if (client.player != null) {
                 if (client.isInSingleplayer() && PlayerData.get.hud.state(
                         Player.of(Objects.requireNonNull(DirectionHUD.server.getPlayerManager().getPlayer(client.player.getUuid()))))) {
-                    client.player.sendMessage(CUtl.tag().append(message).b());
+                    client.player.sendMessage(msg.b());
                 } else if (DirectionHUDClient.onSupportedServer && DirectionHUDClient.hudState) {
-                    client.player.sendMessage(CUtl.tag().append(message).b());
+                    client.player.sendMessage(msg.b());
                 }
             }
         }
